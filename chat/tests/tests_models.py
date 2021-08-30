e7eb2405e722b7fba5b2686e82fdc55ddc8adad4
mohammadT77 <t.mohammadamin@gmail.com>
2021-08-30 21:19:04 +0430

    Test models

---
+++ chat/tests/tests_models.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+# Create your tests here.
+from chat.models import Topic, Message
+
+
+class TopicTest(TestCase):
+
+    def setUp(self) -> None:
+        self.users = [User.objects.create(username='test1'),
+                      User.objects.create(username='test2'),
+                      User.objects.create(username='test3'),
+                      User.objects.create(username='test4')]
+        self.topic = Topic.objects.create(owner=self.users[0], title='Test1')
+        self.topic.participants.add(self.users[1])
+        self.topic.participants.add(self.users[2])
+        self.topic.participants.add(self.users[3])
+
+    def test_add_owner_to_participants(self):
+        self.assertIn(self.users[0],self.topic.participants.all())
+
+
+class MessageTest(TopicTest):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.new_topic = Topic.objects.create(owner=self.users[0], title='Test2')
+        self.topic.participants.add(self.users[1])
+        self.topic.participants.add(self.users[2])
+        self.topic.participants.add(self.users[3])
+
+    def test_send_simple_msg(self):
+        msg1 = Message.objects.create(sender=self.users[0], content='Test1', topic=self.topic)
+        msg2 = Message.objects.create(sender=self.users[1], content='Test2', topic=self.topic)
+        self.assertIn(msg1, self.topic.messages.all())
+        self.assertIn(msg2, self.topic.messages.all())
+
+
+    def test_send_reply_msg(self):
+        msg1 = Message.objects.create(sender=self.users[0], content='Test1', topic=self.topic)
+        msg2 = Message.objects.create(sender=self.users[1], content='Test2', topic=self.topic, reply=msg1)
+        self.assertIn(msg2, msg1.replies.all())
+
+    def test_send_foreign_reply_msg_failed(self):
+        msg1 = Message.objects.create(sender=self.users[0], content='Test1', topic=self.topic)
+        self.assertRaises(Message.ForeignTopicReply, Message.objects.create,
+                          sender=self.users[1], content='Test2', topic=self.new_topic, reply=msg1)
+
+
+
