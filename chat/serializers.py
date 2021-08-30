0a5075a826664d9aa4f0fa616b58b52fad5304e4
mohammadT77 <t.mohammadamin@gmail.com>
2021-08-30 21:24:03 +0430

    Implement Chat serializers.py

---
+++ chat/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from chat.models import Topic, Message
+
+
+class TopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = '__all__'
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
+
