00219a5b0c0f6d4a1506e5b874835a396a27ec42
mohammadT77 <t.mohammadamin@gmail.com>
2021-08-30 22:21:43 +0430

    Add TopicApi

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from chat.views import MessageApi
+from chat.views import MessageApi, TopicApi
 
 urlpatterns = [
     path('message/', MessageApi.as_view()),
+    path('topic/', TopicApi.as_view()),
+    path('topic/<int:pk>', TopicApi.as_view()),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.models import User
+from django.db.models.fields.related_descriptors import ManyToManyDescriptor
 from rest_framework import mixins, permissions
 
 # Create your views here.
 from rest_framework.generics import GenericAPIView
 
-from chat.models import Message
-from chat.serializers import MessageSerializer
+from chat.models import Message, Topic
+from chat.serializers import MessageSerializer, TopicSerializer
 
 
 class MessageApi(mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView):
@@ -21,3 +23,28 @@ class MessageApi(mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView)
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+
+class TopicApi(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, GenericAPIView):
+    serializer_class = TopicSerializer
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    def get_queryset(self):
+        return self.request.user.topics.all()
+
+
+    def get(self, request, *args, **kwargs):
+        if 'pk' in kwargs:
+            return self.retrieve(request, *args, **kwargs)
+        else:
+            return self.list(request, *args, **kwargs)
+
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
